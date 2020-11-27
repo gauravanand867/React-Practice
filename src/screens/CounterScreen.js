4decaf16fc60ac6405fbd022776d8a038f6fef37
gauravanand867 <gauravanand867@gmail.com>
2020-11-27 09:34:42 +0530

    Reducer Finished

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,25 +1,32 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import React, { useReducer } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-const CounterScreen=()=>{
-    const [counter, setCounter]=useState(0);
-    return (
-        <View>
-            <Button 
-            title="Increment"
-            onPress={()=>{
-                setCounter(counter+1)
-            }}/>
-            <Text>Current Count:{counter}</Text>
-            <Button 
-            title="Decrement"
-            onPress={()=>{
-                setCounter(counter-1)
-            }}/>
-        </View>
-    )
-}
+const CounterScreen = () => {
+  const reducer = (state, action) => {
+    return action.type == "Increment"
+      ? { ...state, counter: state.counter + 1 }
+      : { ...state, counter: state.counter - 1 };
+  };
+  const [state, dispatch] = useReducer(reducer, { counter: 0 });
+  return (
+    <View>
+      <Button
+        title="Increment"
+        onPress={() => {
+          dispatch({ type: "Increment" });
+        }}
+      />
+      <Text>Current Count:{state.counter}</Text>
+      <Button
+        title="Decrement"
+        onPress={() => {
+          dispatch({ type: "Decrement" });
+        }}
+      />
+    </View>
+  );
+};
 
-const Style = StyleSheet.create({})
+const Style = StyleSheet.create({});
 
 export default CounterScreen;
